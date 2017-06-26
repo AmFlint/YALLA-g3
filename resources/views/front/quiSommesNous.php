d20e779c01e264b8419edaf51008379de317b555
lacaux-gerome <gerome.lacaux@hetic.net>
2017-06-26 22:36:24 +0200

    Home page et page qui sommes nous responsive

---
+++ resources/views/front/quiSommesNous.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Qui sommes nous ?</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="../../../public/css/bootstrap.css">
+	<link rel="stylesheet" href="../../../public/css/style.css">
+</head>
+<body>
+	<body class="site">
+		<!-- header -->
+		<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="#"><img src="" alt="Logo"> Yalla!</a>
+			<div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Qui some nous ?</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Notre quotidient</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Nos Actus</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Contacts</a>
+					</li>
+					<li class="nav-item">
+						<a href="" class="nav-link">Faire un don</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<!-- Début du contenu au dessus de la ligne de flotaison -->
+		<div class="homePage">
+			<div class="row">
+				<div class="col-xs-12 col-md-12">
+					<h2 class="text-center"><span class="fontWeightBoldDubai">Qui sommes nous ?</span></h2>
+					<p>Yalla! est une association qui aide les enfants Syrien psychologiquement mais également à avoir accès à l’éducation.</p>
+				</div>
+			</div>
+		</div>
+		<div class="contentPageWhoAreYou">
+			<div class="row">
+				<div class="col-xs-12 col-md-5 offset-md-1">
+					<div class="containerImgPageWhoAreYou displayNone">
+						<img class="imgRotatePageWhoAreYou positionnement" src="" alt="">
+						<div class="rectImgColorRed"></div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-5">
+					<p>Yalla ! est dirigée par un Bureau composé de quatre membres, qui rendent compte des actions de Yalla ! aux adhérents de l’association à travers des rapports d’activités concernant chaque projet et lors des Assemblées générales tenues une fois par an à Paris.</p>
+				</div>				
+			</div>
+			<div class="row marginTopContentPageWhoAreYou">
+				<div class="col-xs-12 col-md-4 offset-md-2 orderPlusOne">
+					<p class="">Les bénévoles de Yalla ! à Paris et sur le terrain ont préalablement adopté les valeurs de l’association et agissent conformément à ses principes. Ils rapportent leurs activités de façon régulière au Bureau qui contrôle la conformité des actions développées sur le terrain à la mission et aux valeurs que s’est données Yalla !.</p>
+				</div>
+				<div class="col-xs-12 col-md-5 offset-md-1">
+					<div class="containerImgPageWhoAreYouSecond">
+						<img class="positionnement widthImgSectionTwo" src="" alt="">
+						<div class="rectImgColorPink"></div>
+					</div>
+				</div>				
+			</div>
+			<div class="row">
+				<div class="bannerPageWhoAreYou">
+					<img class="" src="" alt="">
+					<div class="rectImgColorRedBanner"></div>
+				</div>
+			</div>
+			<div class="row justify-content-around marginTopContentPageWhoAreYou">
+				<div class="col-xs-12 col-md-12">
+					<h3 class="text-center styleTitleNotreStategie">Notre stratégie</h3>
+				</div>
+
+				<div class="col-xs-12 col-md-3 ">
+					<img src="" alt="">
+					<h5 class="text-center">Intégration des bénéficiaires</h5>
+					<p class="paddingTopFix">et de la communauté d’accueil à tous les stades de développement du projet afin de garantir une compréhension mutuelle des enjeux et objectifs du projet, et contribuer au succès de la mission.</p>
+				</div>
+				<div class="col-xs-12 col-md-3 ">
+					<img src="" alt="">
+					<h5 class="text-center">Création d’un lien dynamique entre les donateurs et les bénéficiaires</h5>
+					<p class="paddingTopFix">afin de créer une solidarité de proximité et garantir une visibilité sur la destination et l’impact des dons quels que soient leurs montants, tout en faisant parvenir aux bénéficiaires un message de soutien concret de la part de la société civile solidaire.</p>
+				</div>
+				<div class="col-xs-12 col-md-3 ">
+					<img src="" alt="">
+					<h5 class="text-center">Respect de la coordination de l’aide humanitaire</h5>
+					<p class="paddingTopFix">menée par les agences humanitaires des Nations Unies, afin de s’intégrer dans une action globale et parvenir ensemble à plus d’efficacité pour répondre aux besoins les plus prégnants.</p>
+				</div>
+			</div>
+			<div class="row justify-content-around text-center marginTopContentPageWhoAreYou">
+				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
+					<img class="" src="" alt="">
+					<div class="rectImgColorRedStrategieOne"></div>
+					<p>Mary LEMELAND-MELLIONEC, Présidente de l’association</p>
+				</div>
+				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
+					<img src="" alt="">
+					<div class="rectImgColorRedStrategieTwo"></div>
+					<p>Mary LEMELAND-MELLIONEC, Présidente de l’association</p>
+				</div>
+			</div>
+			<div class="row text-center justify-content-around marginTopContentPageWhoAreYou">
+				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
+					<img class="" src="" alt="">
+					<div class="rectImgColorRedStrategieThree"></div>
+					<p>Sophie FANUCCHI, Secrétaire et référente juridique</p>
+				</div>
+				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
+					<img class="" src="" alt="">
+					<div class="rectImgColorRedStrategieFour"></div>
+					<p> Pierre LEMELAND, Trésorier </p>
+				</div>
+			</div>
+			<div class="bannerPageWhoAreYou text-center" style="background-color: #D8D8D8;">
+				<p>Un petit geste à plusieurs, un grand pas ensemble</p>
+				<button class="text-uppercase">faire un don</button>
+			</div>
+		</div>
+
+
+		<!-- footer -->
+
+		<footer>
+			<div class="row rowMarginTopFixCallToAction">
+				<div class="col-xs-6 col-md-12 text-center">
+					<img src="" alt="">
+				</div>
+				<div class="col-xs-6 col-md-12 text-center">
+					<img src="" alt="">
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-12 text-center">
+				<a href="">© Yalla! pour les enfants | une école pour la paix</a>
+			</div>
+			<div class="col-xs-12 col-md-12 text-center">
+				<a href="">13, rue René Villerme – 75011 PARIS</a>
+			</div>
+			<div class="col-xs-12 col-md-12 text-center">
+				<a href="">Mentions légales | Nous contacter</a>
+			</div>
+		</footer>
+	</body>
+	</html>
