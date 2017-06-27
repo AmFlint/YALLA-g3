52a00998789b47efef229a734075e7ccf6bba4f2
AmFlint <amasselot@hotmail.com>
2017-06-27 23:10:27 +0200

    translation about us page fr/en

---
+++ resources/lang/fr/about.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'title' => 'Qui sommes nous ?',
+    'heading_p' => 'Yalla! est une association qui aide les enfants Syrien psychologiquement mais également à avoir accès à l’éducation.',
+    'composition' => 'Yalla! est dirigée par un Bureau composé de quatre membres, qui rendent compte des actions de Yalla! aux adhérents de l’association à travers des rapports d’activités concernant chaque projet et lors des Assemblées générales tenues une fois par an à Paris.',
+    'volunteer' => 'Les bénévoles de Yalla ! à Paris et sur le terrain ont préalablement adopté les valeurs de l’association et agissent conformément à ses principes. Ils rapportent leurs activités de façon régulière au Bureau qui contrôle la conformité des actions développées sur le terrain à la mission et aux valeurs que s’est données Yalla !',
+    'strategy' => 'Notre Stratégie',
+    'benefit' => 'Intégration des bénéficiaires',
+    'benefit_p' => '>et de la communauté d’accueil à tous les stades de développement du projet afin de garantir une compréhension mutuelle des enjeux et objectifs du projet, et contribuer au succès de la mission.',
+    'creation' => 'Création d’un lien dynamique entre les donateurs et les bénéficiaires',
+    'creation_p' => 'afin de créer une solidarité de proximité et garantir une visibilité sur la destination et l’impact des dons quels que soient leurs montants, tout en faisant parvenir aux bénéficiaires un message de soutien concret de la part de la société civile solidaire.',
+    'respect' => 'Respect de la coordination de l’aide humanitaire',
+    'respect_p' => 'menée par les agences humanitaires des Nations Unies, afin de s’intégrer dans une action globale et parvenir ensemble à plus d’efficacité pour répondre aux besoins les plus prégnants.',
+    'team' => 'L’équipe du Bureau',
+
+];
