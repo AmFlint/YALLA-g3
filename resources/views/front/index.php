2160f51dcabad536207d2a99494f990e639dce98
lacaux-gerome <gerome.lacaux@hetic.net>
2017-06-27 16:22:45 +0200

    update html css

--- resources/views/front/index.php
+++ resources/views/front/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="../../../public/css/bootstrap.css">
-<link rel="stylesheet" href="../../../public/css/style.css">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="../../../public/css/bootstrap.css">
+  <link rel="stylesheet" href="../../../public/css/style.css">
 </head>
 <body class="site">
-<!-- header -->
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <!-- header -->
+  <video src="../../../public/video/backgroundHomePage.mp4" autoplay="1" loop="1"></video>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,45 +30,54 @@
           <a href="" class="nav-link">Contacts</a>
         </li>
         <li class="nav-item">
-          <a href="" class="nav-link">Faire un don</a>
+          <div class="dropdown dropdownStyleFix">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Fr
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">En</a>
+              <a class="dropdown-item" href="#">العربية</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item">
+          <button>Faire un don</button>
         </li>
       </ul>
     </div>
   </nav>
 
-<!-- Début du contenu au dessus de la ligne de flotaison -->
-<div class="homePage">
-  <div class="row">
-    <div class="col-xs-12 col-md-12">
-      <h2 class="text-center"> Une école pour la<span class="fontWeightBoldDubai"> paix</span></h2>
-      <button>Faire un don</button>
+  <!-- Début du contenu au dessus de la ligne de flotaison -->
+  <div class="homePage">
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <h2 class="text-center"> Une école pour la<span class="fontWeightBoldDubai"> paix</span></h2>
+        <button>Faire un don</button>
+      </div>
     </div>
   </div>
-</div>
-<div class="contentHomePage">
-  <div class="row">
-    <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
-      <img src="../../../public/img-content/front/homePage1.jpg" alt="">
-      <div class="rectImgColorRed"></div>
+  <div class="contentHomePage">
+    <div class="row">
+      <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
+        <img src="../../../public/img-content/front/homePage1.jpg" alt="">
+        <div class="rectImgColorRed"></div>
+      </div>
+      <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
+        <h3>L'aide humanitaire de proximité</h3>
+        <p>Développer la solidarité internationale en  créant un pont entre les sociétés civiles en  mesure d'aider celles et ceux qui en ont besoin.</p>
+        <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
+      </div>
     </div>
-    <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
-      <h3>L'aide humanitaire de proximité</h3>
-      <p>Développer la solidarité internationale en  créant un pont entre les sociétés civiles en  mesure d'aider celles et ceux qui en ont besoin.</p>
-      <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
-    </div>
-  </div>
-
-
-  <div class="row rowMarginTopFix">
-    <div class="col-xs-12 col-md-5 offset-md-1 alignContainerTextHomePageLeft">
-      <h3>Les droits de l’enfant comme valeurs souveraines</h3>
-      <p>Développer la solidarité internationale en créant un pont entre les sociétés civiles en mesure de d’aider et celles qui en ont besoin.</p>
-      <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
-    </div>
-    <div class="col-xs-12 col-md-5 paddingFix orderContentHomePage">
-      <img src="../../../public/img-content/front/homePage2.jpg" alt="">
-      <div class="rectImgColorYellow"></div>
-    </div>
+    <div class="row rowMarginTopFix">
+      <div class="col-xs-12 col-md-5 offset-md-1 alignContainerTextHomePageLeft">
+        <h3>Les droits de l’enfant comme valeurs souveraines</h3>
+        <p>Développer la solidarité internationale en créant un pont entre les sociétés civiles en mesure de d’aider et celles qui en ont besoin.</p>
+        <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
+      </div>
+      <div class="col-xs-12 col-md-5 paddingFix orderContentHomePage">
+        <img src="../../../public/img-content/front/homePage2.jpg" alt="">
+        <div class="rectImgColorYellow"></div>
+      </div>
     </div>
 
     <div class="row rowMarginTopFix">
@@ -84,17 +94,35 @@
     </div>
   </div> 
   <div class="row rowMarginTopFixCallToAction">
+    <div class="col-md-12 offset-md-12">
+      <h3 class="text-center">Notre dernière actualité</h3>
+    </div>
     <div class="col-md-4 offset-md-4 col-xs-12 text-center">
       <img src="../../../public/img-content/front/homePage4.jpg" alt="">
       <h4 class="marginTopAddTag">Fest-noz solidaire le 29 octobre à Poullaouen</h4>
       <p class="marginTopAddTag">Yalla! Pour les Enfants vous convie le samedi 29 octobre à un fest-noz de levée de fonds dont les entiers bénéfices reviendront à son école d’Aley, située au Liban, à quelques kilomètres</p>
       <div>
-      <button class="text-uppercase">lire l'article</button>
+        <button class="text-uppercase">lire l'article</button>
       </div>
       <a class="marginTopAddTag hoverFix" href="">Voir tous nos articles</a>
     </div>
   </div>
-
+  <div class="row rowMarginTopFixCallToAction">
+    <div class="col-md-4 offset-md-2 col-xs-12 card">
+      <img src="../../../public/img-layout/facebook.svg" alt="">
+      <div class="col-md-8 offset-md-2">
+        <p>Yalla - Pour les Enfants <span>23 juin, 14:09</span></p>
+        <p>42 of yalla students at indstitut francais du Liban watching movies.</p>
+      </div>
+    </div>
+    <div class="col-md-4 offset-md-1 col-xs-12 card">
+      <img src="../../../public/img-layout/twitter.svg" alt="">
+      <div class="col-md-8 offset-md-2">
+        <p>Yalla!pourlesenfants <span>24 juin</span></p>
+        <p>Weekly meeting at the center on child protection with our Child Protection Adviser.</p>
+      </div>
+    </div>
+  </div>
   <div class="row text-center rowMarginTopFixCallToAction">
     <div class="col-xs-12 col-md-12 buttonCTA backgroundBannerHommePageOne">
       <p style="margin-top: 14vh;">Un petit geste à <span class="fontWeightBoldDubai">plusieurs</span> un grand pas <span class="fontWeightBoldDubai">ensemble</span></p>
