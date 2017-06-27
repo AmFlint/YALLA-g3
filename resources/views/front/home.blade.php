6e95955a209c6be800ba76d3c008183db64f1510
AmFlint <amasselot@hotmail.com>
2017-06-27 17:29:04 +0200

    update inte.html to blade.php with layout

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,69 +1,95 @@
 @extends('front.layout_front')
-@section('meta')
-    <meta name="description" content="{{Lang::get('meta.home_description')}}">
-@endsection
+
 @section('body_class', 'site')
 @section('content')
-<!-- Début du contenu au dessus de la ligne de flotaison -->
-<div class="homePage">
-  <div class="row">
-    <div class="col-xs-12 col-md-12">
-      <h2 class="text-center"> Une école pour la<span class="fontWeightBoldDubai"> paix</span></h2>
-      <button>Faire un don</button>
+  <!-- header -->
+  <video src="{{asset('video/backgroundHomePage.mp4')}}" autoplay="1" loop="1"></video>
+  <!-- Début du contenu au dessus de la ligne de flotaison -->
+  <div class="homePage">
+    <div class="row">
+      <div class="col-xs-12 col-md-12">
+        <h2 class="text-center"> Une école pour la<span class="fontWeightBoldDubai"> paix</span></h2>
+        <button>Faire un don</button>
+      </div>
     </div>
   </div>
-</div>
-<div class="contentHomePage">
-  <div class="row">
-    <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
-      <img src="" alt="">
-      <div class="rectImgColorRed"></div>
+  <div class="contentHomePage">
+    <div class="row">
+      <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
+        <img src="{{asset('img-content/front/homePage1.jpg')}}" alt="">
+        <div class="rectImgColorRed"></div>
+      </div>
+      <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
+        <h3>L'aide humanitaire de proximité</h3>
+        <p>Développer la solidarité internationale en  créant un pont entre les sociétés civiles en  mesure d'aider celles et ceux qui en ont besoin.</p>
+        <img src="{{asset('img-layout/picto-plus-copy.svg')}}" alt="">
+      </div>
     </div>
-    <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
-      <h3>L'aide humanitaire de proximité</h3>
-      <p>Développer la solidarité internationale en  créant un pont entre les sociétés civiles en  mesure d'aider celles et ceux qui en ont besoin.</p>
-    </div>
-  </div>
-
-
-  <div class="row rowMarginTopFix">
-    <div class="col-xs-12 col-md-5 offset-md-1 alignContainerTextHomePageLeft">
-      <h3>Les droits de l’enfant comme valeurs souveraines</h3>
-      <p>Développer la solidarité internationale en créant un pont entre les sociétés civiles en mesure de d’aider et celles qui en ont besoin.</p>
-    </div>
-    <div class="col-xs-12 col-md-5 paddingFix orderContentHomePage">
-      <img src="" alt="">
-      <div class="rectImgColorYellow"></div>
-    </div>
+    <div class="row rowMarginTopFix">
+      <div class="col-xs-12 col-md-5 offset-md-1 alignContainerTextHomePageLeft">
+        <h3>Les droits de l’enfant comme valeurs souveraines</h3>
+        <p>Développer la solidarité internationale en créant un pont entre les sociétés civiles en mesure de d’aider et celles qui en ont besoin.</p>
+        <img src="{{asset('img-layout/picto-plus-copy.svg')}}" alt="">
+      </div>
+      <div class="col-xs-12 col-md-5 paddingFix orderContentHomePage">
+        <img src="{{asset('img-content/front/homePage2.jpg')}}" alt="">
+        <div class="rectImgColorYellow"></div>
+      </div>
     </div>
 
     <div class="row rowMarginTopFix">
       <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
-        <img src="" alt="">
+        <img src="{{asset('img-content/front/homePage3.jpg')}}" alt="">
         <div class="rectImgColorGreen">
         </div>
       </div>
       <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
         <h3>Notre histoire</h3>
         <p>Yalla! Pour les Enfants a été créée en juillet 2013 par des citoyens français connaisseurs de la Syrie et du Moyen-Orient, et soucieux du respect des droits l’Homme.</p>
+        <img src="{{asset('img-layout/picto-plus-copy.svg')}}" alt="">
+      </div>
+    </div>
+  </div> 
+  <div class="row rowMarginTopFixCallToAction">
+    <div class="col-md-12 offset-md-12">
+      <h3 class="text-center">Notre dernière actualité</h3>
+    </div>
+    <div class="col-md-4 offset-md-4 col-xs-12 text-center">
+      <img src="{{asset('img-content/front/homePage4.jpg')}}" alt="">
+      <h4 class="marginTopAddTag">Fest-noz solidaire le 29 octobre à Poullaouen</h4>
+      <p class="marginTopAddTag">Yalla! Pour les Enfants vous convie le samedi 29 octobre à un fest-noz de levée de fonds dont les entiers bénéfices reviendront à son école d’Aley, située au Liban, à quelques kilomètres</p>
+      <div>
+        <button class="text-uppercase">lire l'article</button>
+      </div>
+      <a class="marginTopAddTag hoverFix" href="">Voir tous nos articles</a>
+    </div>
+  </div>
+  <div class="row rowMarginTopFixCallToAction">
+    <div class="col-md-4 offset-md-2 col-xs-12 card">
+      <img src="{{asset('img-layout/facebook.svg')}}" alt="">
+      <div class="col-md-8 offset-md-2">
+        <p>Yalla - Pour les Enfants <span>23 juin, 14:09</span></p>
+        <p>42 of yalla students at indstitut francais du Liban watching movies.</p>
+      </div>
+    </div>
+    <div class="col-md-4 offset-md-1 col-xs-12 card">
+      <img src="{{asset('img-layout/twitter.svg')}}" alt="">
+      <div class="col-md-8 offset-md-2">
+        <p>Yalla!pourlesenfants <span>24 juin</span></p>
+        <p>Weekly meeting at the center on child protection with our Child Protection Adviser.</p>
       </div>
     </div>
   </div>
-
-
-
-
-
   <div class="row text-center rowMarginTopFixCallToAction">
-    <div class="col-xs-12 col-md-12">
-      <h4>Ensemble rendons le monde meilleur</h4>
-      <button class="text-uppercase callToAction">Nous rejoindre</button>
+    <div class="col-xs-12 col-md-12 buttonCTA backgroundBannerHommePageOne">
+      <p style="margin-top: 14vh;">Un petit geste à <span class="fontWeightBoldDubai">plusieurs</span> un grand pas <span class="fontWeightBoldDubai">ensemble</span></p>
+      <button>Faire un don</button>
     </div>
   </div>
-  <div class="row text-center rowMarginTopFixCallToAction">
-    <div class="col-xs-12 col-md-12">
-      <h4>Aidez-nous à rendre le monde meilleur</h4>
-      <button class="text-uppercase callToAction">Faire un don</button>
+  <div class="row text-center">
+    <div class="col-xs-12 col-md-12 buttonCTA backgroundBannerHommePage2">
+      <p style="margin-top: 14vh;">Devenez <span class="fontWeightBoldDubai">membre</span> de ce projet</p>
+      <button>Nous rejoindre</button>
     </div>
   </div>
 </div>
--- resources/views/front/layout_front.blade.php
+++ resources/views/front/layout_front.blade.php
@@ -6,13 +6,15 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    @yield('style')
 </head>
 <body class="@yield('body_class')">
+@yield('nav')
 <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#"><img src="" alt="Logo"> Yalla!</a>
+    <a class="navbar-brand" href="#"><img src="../../../public/img-layout/logo.png" alt="Logo"></a>
     <div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
         <ul class="navbar-nav">
             <li class="nav-item">
@@ -22,18 +24,28 @@
                 <a class="nav-link" href="#">Notre quotidien</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Nos Actus</a>
+                <a class="nav-link" href="{{route('post_listing')}}">Nos Actualités</a>
             </li>
             <li class="nav-item">
                 <a href="" class="nav-link">Contact</a>
             </li>
             <li class="nav-item">
-                <a href="" class="nav-link">Faire un don</a>
+                <div class="dropdown dropdownStyleFix">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Fr
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">En</a>
+                        <a class="dropdown-item" href="#">العربية</a>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item">
+                <button>Faire un don</button>
             </li>
         </ul>
     </div>
 </nav>
-
 @yield('content')
 
 <footer>
@@ -46,18 +58,20 @@
         </div>
     </div>
     <div class="col-xs-12 col-md-12 text-center">
-        <a href="">© Yalla! pour les enfants | une école pour la paix</a>
+        <a class="hoverFix" href="">© Yalla! pour les enfants | une école pour la paix</a>
     </div>
     <div class="col-xs-12 col-md-12 text-center">
-        <a href="">13, rue René Villerme – 75011 PARIS</a>
+        <a class="hoverFix" href="">13, rue René Villerme – 75011 PARIS</a>
     </div>
     <div class="col-xs-12 col-md-12 text-center">
-        <a href="">Mentions légales | Nous contacter</a>
+        <a class="hoverFix" href="">Mentions légales | Nous contacter</a>
     </div>
 </footer>
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
 @yield('scripts')
 </body>
 </html>
