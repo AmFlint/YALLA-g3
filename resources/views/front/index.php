b9f6beb130ea85c9c5f5617f5db150e23ac26301
lacaux-gerome <gerome.lacaux@hetic.net>
2017-06-27 15:17:17 +0200

    update html css

--- resources/views/front/index.php
+++ resources/views/front/index.php
@@ -10,30 +10,30 @@
 <body class="site">
 <!-- header -->
 <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#"><img src="" alt="Logo"> Yalla!</a>
-  <div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Qui some nous ?</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Notre quotidient</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Nos Actus</a>
-      </li>
-      <li class="nav-item">
-        <a href="" class="nav-link">Contacts</a>
-      </li>
-      <li class="nav-item">
-        <a href="" class="nav-link">Faire un don</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"><img src="../../../public/img-layout/logo.png" alt="Logo"></a>
+    <div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Qui some nous ?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Notre quotidient</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nos Actus</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Contacts</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">Faire un don</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
 <!-- Début du contenu au dessus de la ligne de flotaison -->
 <div class="homePage">
@@ -47,12 +47,13 @@
 <div class="contentHomePage">
   <div class="row">
     <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
-      <img src="" alt="">
+      <img src="../../../public/img-content/front/homePage1.jpg" alt="">
       <div class="rectImgColorRed"></div>
     </div>
     <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
       <h3>L'aide humanitaire de proximité</h3>
       <p>Développer la solidarité internationale en  créant un pont entre les sociétés civiles en  mesure d'aider celles et ceux qui en ont besoin.</p>
+      <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
     </div>
   </div>
 
@@ -61,23 +62,36 @@
     <div class="col-xs-12 col-md-5 offset-md-1 alignContainerTextHomePageLeft">
       <h3>Les droits de l’enfant comme valeurs souveraines</h3>
       <p>Développer la solidarité internationale en créant un pont entre les sociétés civiles en mesure de d’aider et celles qui en ont besoin.</p>
+      <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
     </div>
     <div class="col-xs-12 col-md-5 paddingFix orderContentHomePage">
-      <img src="" alt="">
+      <img src="../../../public/img-content/front/homePage2.jpg" alt="">
       <div class="rectImgColorYellow"></div>
     </div>
     </div>
 
     <div class="row rowMarginTopFix">
       <div class="col-xs-12 col-md-5 offset-md-1 paddingFix">
-        <img src="" alt="">
+        <img src="../../../public/img-content/front/homePage3.jpg" alt="">
         <div class="rectImgColorGreen">
         </div>
       </div>
       <div class="col-xs-12 col-md-5 alignContainerTextHomePageRight desktopText">
         <h3>Notre histoire</h3>
         <p>Yalla! Pour les Enfants a été créée en juillet 2013 par des citoyens français connaisseurs de la Syrie et du Moyen-Orient, et soucieux du respect des droits l’Homme.</p>
+        <img src="../../../public/img-layout/picto-plus-copy.svg" alt="">
       </div>
+    </div>
+  </div> 
+  <div class="row rowMarginTopFixCallToAction">
+    <div class="col-md-4 offset-md-4 col-xs-12 text-center">
+      <img src="../../../public/img-content/front/homePage4.jpg" alt="">
+      <h4 class="marginTopAddTag">Fest-noz solidaire le 29 octobre à Poullaouen</h4>
+      <p class="marginTopAddTag">Yalla! Pour les Enfants vous convie le samedi 29 octobre à un fest-noz de levée de fonds dont les entiers bénéfices reviendront à son école d’Aley, située au Liban, à quelques kilomètres</p>
+      <div>
+      <button class="text-uppercase">lire l'article</button>
+      </div>
+      <a class="marginTopAddTag hoverFix" href="">Voir tous nos articles</a>
     </div>
   </div>
 
@@ -88,13 +102,13 @@
   <div class="row text-center rowMarginTopFixCallToAction">
     <div class="col-xs-12 col-md-12">
       <h4>Ensemble rendons le monde meilleur</h4>
-      <button class="text-uppercase callToAction">Nous rejoindre</button>
+      <button>Nous rejoindre</button>
     </div>
   </div>
   <div class="row text-center rowMarginTopFixCallToAction">
     <div class="col-xs-12 col-md-12">
       <h4>Aidez-nous à rendre le monde meilleur</h4>
-      <button class="text-uppercase callToAction">Faire un don</button>
+      <button>Faire un don</button>
     </div>
   </div>
 </div>
@@ -109,13 +123,13 @@
     </div>
   </div>
   <div class="col-xs-12 col-md-12 text-center">
-    <a href="">© Yalla! pour les enfants | une école pour la paix</a>
+    <a class="hoverFix" href="">© Yalla! pour les enfants | une école pour la paix</a>
   </div>
   <div class="col-xs-12 col-md-12 text-center">
-    <a href="">13, rue René Villerme – 75011 PARIS</a>
+    <a class="hoverFix" href="">13, rue René Villerme – 75011 PARIS</a>
   </div>
   <div class="col-xs-12 col-md-12 text-center">
-    <a href="">Mentions légales | Nous contacter</a>
+    <a class="hoverFix" href="">Mentions légales | Nous contacter</a>
   </div>
 </footer>
 
--- resources/views/front/quiSommesNous.php
+++ resources/views/front/quiSommesNous.php
@@ -9,42 +9,55 @@
 </head>
 <body>
 	<body class="site">
-		<!-- header -->
-		<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<a class="navbar-brand" href="#"><img src="" alt="Logo"></a>
-			<div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="#">Qui some nous ?</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Notre quotidient</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Nos Actus</a>
-					</li>
-					<li class="nav-item">
-						<a href="" class="nav-link">Contacts</a>
-					</li>
-					<li class="nav-item">
-						<a href="" class="nav-link">Faire un don</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-
-		<!-- Début du contenu au dessus de la ligne de flotaison -->
-		<div class="homePage">
-			<div class="row">
-				<div class="col-xs-12 col-md-12">
-					<h2 class="text-center"><span class="fontWeightBoldDubai">Qui sommes nous ?</span></h2>
-					<p>Yalla! est une association qui aide les enfants Syrien psychologiquement mais également à avoir accès à l’éducation.</p>
+		<div class="quiSommesNousBg">
+			<!-- header -->
+			<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="../../../public/img-layout/logo.png" alt="Logo"></a>
+				<div class="collapse navbar-collapse text-uppercase justify-content-end" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Qui some nous ?</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Notre quotidient</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Nos Actus</a>
+						</li>
+						<li class="nav-item">
+							<a href="" class="nav-link">Contacts</a>
+						</li>
+						<li class="nav-item">
+							<div class="dropdown dropdownStyleFix">
+								<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Fr
+								</button>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a class="dropdown-item" href="#">En</a>
+									<a class="dropdown-item" href="#">العربية</a>
+								</div>
+							</div>
+						</li>
+						<li class="nav-item">
+							<button>Faire un don</button>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<!-- Début du contenu au dessus de la ligne de flotaison -->
+			<div class="homePage">
+				<div class="row">
+					<div class="col-xs-12 col-md-8 offset-md-2">
+						<h2 class="text-center"><span class="fontWeightBoldDubai">Qui sommes nous ?</span></h2>
+						<p>Yalla! est une association qui aide les enfants Syrien psychologiquement mais également à avoir accès à l’éducation.</p>
+					</div>
 				</div>
 			</div>
 		</div>
+		<!-- Début du contenu en dessous de la ligne de floataison -->
 		<div class="contentPageWhoAreYou">
 			<div class="row">
 				<div class="col-xs-12 col-md-5 offset-md-1">
@@ -63,15 +76,14 @@
 				</div>
 				<div class="col-xs-12 col-md-5 offset-md-1">
 					<div class="containerImgPageWhoAreYouSecond">
-						<img class="positionnement widthImgSectionTwo" src="" alt="">
+						<img class="positionnement widthImgSectionTwo" src="../../../public/img-content/front/quiSommesNous2.jpg" alt="">
 						<div class="rectImgColorPink"></div>
 					</div>
 				</div>				
 			</div>
 			<div class="row">
-				<div class="bannerPageWhoAreYou">
-					<img class="" src="" alt="">
-					<div class="rectImgColorRedBanner"></div>
+				<div class="bannerPageWhoAreYouFirst">
+					<!-- <div class="rectImgColorYellowBanner"></div> -->
 				</div>
 			</div>
 			<div class="row justify-content-around marginTopContentPageWhoAreYou">
@@ -79,48 +91,49 @@
 					<h3 class="text-center styleTitleNotreStategie">Notre stratégie</h3>
 				</div>
 
-				<div class="col-xs-12 col-md-3 ">
-					<img src="" alt="">
+				<div class="col-xs-12 col-md-3">
+					<img class="alignBottomClassic" src="../../../public/img-layout/class-group-of-students-and-professor-in-front.svg" alt="">
 					<h5 class="text-center">Intégration des bénéficiaires</h5>
 					<p class="paddingTopFix">et de la communauté d’accueil à tous les stades de développement du projet afin de garantir une compréhension mutuelle des enjeux et objectifs du projet, et contribuer au succès de la mission.</p>
 				</div>
-				<div class="col-xs-12 col-md-3 ">
-					<img src="" alt="">
+				<div class="col-xs-12 col-md-3">
+					<img class="alignBottomClassic" src="../../../public/img-layout/world.svg" alt="">
 					<h5 class="text-center">Création d’un lien dynamique entre les donateurs et les bénéficiaires</h5>
 					<p class="paddingTopFix">afin de créer une solidarité de proximité et garantir une visibilité sur la destination et l’impact des dons quels que soient leurs montants, tout en faisant parvenir aux bénéficiaires un message de soutien concret de la part de la société civile solidaire.</p>
 				</div>
-				<div class="col-xs-12 col-md-3 ">
-					<img src="" alt="">
+				<div class="col-xs-12 col-md-3">
+					<img class="alignBottomClassic" src="../../../public/img-layout/link.svg" alt="">
 					<h5 class="text-center">Respect de la coordination de l’aide humanitaire</h5>
 					<p class="paddingTopFix">menée par les agences humanitaires des Nations Unies, afin de s’intégrer dans une action globale et parvenir ensemble à plus d’efficacité pour répondre aux besoins les plus prégnants.</p>
 				</div>
 			</div>
-			<div class="row justify-content-around text-center marginTopContentPageWhoAreYou">
-				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
-					<img class="" src="" alt="">
+			<h3 class="col-xs-12 col-md-12 text-center marginTopContentPageWhoAreYou styleTitleNotreStategie">L’équipe du Bureau</h3>
+			<div class="row text-center marginTopContentPageWhoAreYou">
+				<div class="col-xs-12 offset-xs-0 col-md-3 offset-md-2 containerImgNotreStrategie">
+					<img class="" src="../../../public/img-content/front/quiSommesNous4.jpg" alt="">
 					<div class="rectImgColorRedStrategieOne"></div>
 					<p>Mary LEMELAND-MELLIONEC, Présidente de l’association</p>
 				</div>
-				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
-					<img src="" alt="">
+				<div class="col-xs-12 offset-xs-0 col-md-3 offset-md-2 containerImgNotreStrategie">
+					<img src="../../../public/img-content/front/quiSommesNous5.jpg" alt="">
 					<div class="rectImgColorRedStrategieTwo"></div>
 					<p>Mary LEMELAND-MELLIONEC, Présidente de l’association</p>
 				</div>
 			</div>
-			<div class="row text-center justify-content-around marginTopContentPageWhoAreYou">
-				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
-					<img class="" src="" alt="">
+			<div class="row text-center marginTopContentPageWhoAreYou">
+				<div class="col-xs-10 offset-xs-0 col-md-3 offset-md-2 containerImgNotreStrategie">
+					<img class="" src="../../../public/img-content/front/quiSommesNous6.jpg" alt="">
 					<div class="rectImgColorRedStrategieThree"></div>
 					<p>Sophie FANUCCHI, Secrétaire et référente juridique</p>
 				</div>
-				<div class="col-xs-12 col-md-3 containerImgNotreStrategie">
-					<img class="" src="" alt="">
+				<div class="col-xs-12 offset-xs-0 col-md-3 offset-md-2 containerImgNotreStrategie">
+					<img class="" src="../../../public/img-content/front/quiSommesNous7.jpg" alt="">
 					<div class="rectImgColorRedStrategieFour"></div>
 					<p> Pierre LEMELAND, Trésorier </p>
 				</div>
 			</div>
-			<div class="bannerPageWhoAreYou text-center" style="background-color: #D8D8D8;">
-				<p>Un petit geste à plusieurs, un grand pas ensemble</p>
+			<div class="bannerPageWhoAreYou text-center">
+				<p>Un petit geste à <span class="fontWeightBoldDubai">plusieurs</span>, un grand pas <span class="fontWeightBoldDubai">ensemble</span></p>
 				<button class="text-uppercase">faire un don</button>
 			</div>
 		</div>
@@ -129,22 +142,22 @@
 		<!-- footer -->
 
 		<footer>
-			<div class="row rowMarginTopFixCallToAction">
-				<div class="col-xs-6 col-md-12 text-center">
-					<img src="" alt="">
+			<div class="row rowMarginTopFixCallToAction alignTopFooter">
+				<div class="col-xs-6 col-md-1 offset-md-5 text-center">
+					<img src="../../../public/img-layout/twitter.svg" alt="">
 				</div>
-				<div class="col-xs-6 col-md-12 text-center">
-					<img src="" alt="">
+				<div class="col-xs-6 col-md-1 text-center">
+					<img src="../../../public/img-layout/facebook.svg" alt="">
 				</div>
 			</div>
-			<div class="col-xs-12 col-md-12 text-center">
-				<a href="">© Yalla! pour les enfants | une école pour la paix</a>
+			<div class="col-xs-12 col-md-12 text-center alignBottomClassic">
+				<a class="hoverFix" href="">© Yalla! pour les enfants | une école pour la paix</a>
 			</div>
-			<div class="col-xs-12 col-md-12 text-center">
-				<a href="">13, rue René Villerme – 75011 PARIS</a>
+			<div class="col-xs-12 col-md-12 text-center alignBottomClassic">
+				<a class="hoverFix" href="">13, rue René Villerme – 75011 PARIS</a>
 			</div>
-			<div class="col-xs-12 col-md-12 text-center">
-				<a href="">Mentions légales | Nous contacter</a>
+			<div class="col-xs-12 col-md-12 text-center alignBottomClassic">
+				<a class="hoverFix" href="">Mentions légales | Nous contacter</a>
 			</div>
 		</footer>
 	</body>
