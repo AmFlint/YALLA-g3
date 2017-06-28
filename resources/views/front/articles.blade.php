0a5f95de6e98e3725f5a62f5a1c04423f021961b
YorelN <nicolas.leroy971@hetic.net>
2017-06-29 00:57:51 +0200

    page liste article

--- resources/views/front/articles.blade.php
+++ resources/views/front/articles.blade.php
@@ -1,78 +1,47 @@
 @extends('front.layout_front')
 @section('content')
   {{-- A virer --}}
-  <link rel="stylesheet" href="{{asset('css/screen-2.css')}}">
+    <link rel="stylesheet" href="{{asset('css/screen-2.css')}}">
   {{-- --}}
-    <h2 class="text-center">Nos informations</h2>
+    <h2 class="text-center" style="margin: 5% auto">Nos informations</h2>
 
-    <div class="row justify-content-around">
-    <div class="col-xs-12 col-md-4 col-lg-4 text-center column">
-      <a href="#">
-      <div class="square1">
-        <img class="img-fluid center-block rounded" src="{{asset('img-content/front/article-1.png')}}" alt="">
-        <div class="square-width">
-        <h3 class="text-center h3-articles">Fest-noz solidaire le 29 octobre à Poullaouen</h3>
-        <p class="text-center p-articles col-xs-10 offset-md-1 col-md-10 col-lg-10">Yalla! Pour les Enfants vous convie le samedi 29 octobre à un fest-noz de levée de fonds dont les entiers bénéfices reviendront à son école d’Aley, située au Liban, à quelques kilomètres.</p>
-        <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
+<div class="container clearfix">
+  <div class="col-md-12">
+      <div class="artImg">
+          <img class="img-fluid center-block" src="{{asset('img-content/front/article-3.png')}}" alt="">
+          <div class="square-width actuContent" id="actuContent">
+              <h3 class="h3-articles">Assemblée Générale de Yalla ! Pour les Enfants.</h3>
+              <p class="p-articles col-xs-10 col-md-10 col-lg-10">Chers adhérents, chers amis, Nous vous invitons à l’Assemblée Générale de Yalla ! Pour les Enfants, qui se tiendra le jeudi 29 septembre 2016 à 18h30 à « La Trockette » 125, rue du Chemin Vert.</p>
+              <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
+          </div>
+          <div id="rectActuYellow"></div>
       </div>
-    </div>
-  </a>
   </div>
-
-    <div class="col-xs-12 col-md-4 col-lg-4 text-center column">
-      <a href="#">
-      <div class="square2">
-      <img class="img-fluid center-block rounded" src="{{asset('img-content/front/article-2.png')}}" alt="">
-      <div class="square-width">
-      <h3 class="text-center h3-articles">Assemblée Générale de Yalla ! Pour les Enfants.</h3>
-      <p class="text-center p-articles col-xs-10 offset-md-1 col-md-10 col-lg-10">Chers adhérents, chers amis, Nous vous invitons à l’Assemblée Générale de Yalla ! Pour les Enfants, qui se tiendra le jeudi 29 septembre 2016 à 18h30 à « La Trockette » 125, rue du Chemin Vert.</p>
-      <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
-    </div>
-    </div>
-  </a>
-    </div>
-  </div>
-
-    <div class="row justify-content-between">
-    <div class="col-xs-12 col-md-4 col-lg-4 text-center column">
-      <a href="#">
-      <div class="square2">
-      <img class="img-fluid center-block rounded" src="{{asset('img-content/front/article-3.png')}}" alt="">
-      <div class="square-width">
-      <h3 class="text-center h3-articles">Pétition « Ban Ki-moon :<br> STOPPONS LA GUERRE EN SYRIE »</h3>
-      <p class="text-center p-articles col-xs-10 offset-md-1 col-md-10 col-lg-10">Nous relayons l’appel citoyen adressé au Secrétaire Général des Nations Unies Ban Ki-moon. Pour signer cette pétition, cliquez ici.</p>
-      <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
-    </div>
-    </div>
-  </a>
+    <div class="col-md-12">
+        <div class="artImg">
+            <img class="img-fluid center-block" src="{{asset('img-content/front/article-3.png')}}" alt="">
+            <div class="square-width" id="actuContent" style="border-bottom: 10px solid #cd3d41">
+                <h3 class="h3-articles">Assemblée Générale de Yalla ! Pour les Enfants.</h3>
+                <p class="p-articles col-xs-10 col-md-10 col-lg-10">Chers adhérents, chers amis, Nous vous invitons à l’Assemblée Générale de Yalla ! Pour les Enfants, qui se tiendra le jeudi 29 septembre 2016 à 18h30 à « La Trockette » 125, rue du Chemin Vert.</p>
+                <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
+            </div>
+            <div id="rectActuRed"></div>
+        </div>
     </div>
 
-    <div class="col-xs-12 col-md-4 col-lg-4 text-center column">
-      <a href="#">
-      <div class="square1">
-      <img class="img-fluid center-block rounded" src="{{asset('img-content/front/article-4.png')}}" alt="">
-      <div class="square-width">
-      <h3 class="text-center h3-articles">Les prochains évènements de<br> Souria Houria</h3>
-      <p class="text-center p-articles col-xs-10 offset-md-1 col-md-10 col-lg-10">Découvrez ici les nombreuses rencontres proposées par l’association Souria Houria dans les semaines qui viennent.</p>
-      <button type="button" class="btn btn-secondary btn-articles"><a href="#">Lire l'article</a></button>
-    </div>
-    </div>
-  </a>
-  </div>
 </div>
-
-  <nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-center column">
-    <li class="page-item"><a class="page-link pagination active-pagination test-hover" href="#">1</a></li>
-    <li class="page-item"><a class="page-link pagination test-hover" href="#">2</a></li>
-    <li class="page-item"><a class="page-link pagination test-hover" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link pagination test-hover" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-        <span class="sr-only">Next</span>
-      </a>
-    </li>
-  </ul>
-</nav>
+      <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center column">
+        <li class="page-item"><a class="page-link pagination active-pagination test-hover" href="#">1</a></li>
+        <li class="page-item"><a class="page-link pagination test-hover" href="#">2</a></li>
+        <li class="page-item"><a class="page-link pagination test-hover" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link pagination test-hover" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
 
 @endsection
