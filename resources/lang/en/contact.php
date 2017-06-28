920f70ad1292b7a139b9d6d315c0acdde4f214c0
AmFlint <amasselot@hotmail.com>
2017-06-28 16:32:58 +0200

    update translations contact en/fr + migration views / month

---
+++ resources/lang/en/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Send us a message !',
+    'meta_description' => 'Question? You can ask it to us directly via message and we\'ll do our best to answer it !',
+    'listen' => 'All eyes on you :',
+    'question' => 'Your question, idea, soft words...',
+    'greetings' => 'Greetings,',
+    'name' => 'Your name :',
+    'mail' => 'Your mail address',
+];
