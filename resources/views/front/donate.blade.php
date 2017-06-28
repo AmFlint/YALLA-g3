41f7cbdb4f909c87f96ed929b8384532e5c3ba7b
YorelN <nicolas.leroy971@hetic.net>
2017-06-28 13:16:52 +0200

    page don maj

--- resources/views/front/donate.blade.php
+++ resources/views/front/donate.blade.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="{{asset('css/screen-2.css')}}">
   <h1 class="text-center h1-don">Nous soutenir</h1>
   <div class="offset-md-1">
-  <h3>Pour faire un don à Yalla!</h3>
+  <h2>Pour faire un don à Yalla!</h2>
 </div>
   <div class="row margin">
     <div class="col-md-3 col-xs-12 col-lg-3 offset-md-1">
@@ -17,24 +17,23 @@
     </div>
     <div class="col-md-6 col-xs-12 col-lg-6 offset-md-1">
       <img class="img-fluid rounded" src="{{asset('img-content/front/don-1.png')}}" alt="">
+      <div class="rectImgColorYellowDonate"></div>
     </div>
   </div>
+    <h2 class="h3-don text-center" style="font-size: 2.5em">L'importance des dons réguliers</h2>
 
-  <div class="offset-md-8">
-    <h3 class="h3-don">L'importance des dons réguliers</h3>
-  </div>
-  <div class="row">
-
-      <div class="col-md-6 col-xs-12 col-lg-6 offset-md-1">
-        <img class="img-fluid rounded no-active" src="{{asset('img-content/front/don-2.png')}}" alt="">
+    <div class="container">
+      <div class="col-md-12 col-xs-12 col-lg-12 row">
+        <img class="img-fluid rounded" src="{{asset('img-content/front/don-2.png')}}" alt="" style="position: relative">
+        <div class="rectImgColorGreenDonate"></div>
       </div>
-      <div class="col-md-3 col-xs-12 col-lg-3 offset-md-1">
-        <ul>
+      <div class="col-md-12 col-xs-12 col-lg-12 row">
+        <ul class="col-md-6 margin" style="border-right: 1px solid lightgrey; line-height: 200%">
           <li>Pour répondre à nombreuses de vos questions, nous n’organisons pas de parrainage individuel et personnalisé en raison de nos ressources, limitées, et de notre principe d’équité, de ne pas faire de différence entre nos écoliers. Toutefois, le don régulier de nos adhérents et sympathisants n’en reste pas moins crucial pour le bon déroulement de l’année scolaire. il permettra aux enfants d’avoir une scolarité ininterrompue, ce qui est indispensable à son équilibre et à sa progression. En donnant 15 euros par mois, vous couvrez les frais de scolarisation d’un enfant syrien pendant une semaine.</li>
           <li class="margin">Il suffit pour cela d’autoriser un virement automatique de votre compte en banque sur celui de Yalla! en indiquant en objet : “Parrainage Yalla!”</li>
         </ul>
 
-        <ul class="font-bold">
+        <ul class="font-bold col-md-6 margin col-lg-6 col-xs-12">
           <li>Coordonnées bancaires de Yalla! Pour les Enfants</li>
           <li class="margin">CREDIT COOPERATIF</li>
           <li class="margin">RIB : 42559 00005 41020040979 04</li>
